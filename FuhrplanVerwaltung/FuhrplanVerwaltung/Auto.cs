0a60859a49e41c23686a53d8f5eb90246ea0d5b6
Chefsalah <salahadin.a@gso.schule.koeln>
2021-11-21 02:53:25 +0100

    first commit

---
+++ FuhrplanVerwaltung/FuhrplanVerwaltung/Auto.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FuhrplanVerwaltung
+{
+    public class Auto
+    {
+        private int kilometerstand;
+        private double verbrauchInLiternpro100K;
+        private double tankinhalt = 100;
+
+        int i = 0;
+
+        public Auto(int kilometerstand)
+        {
+            this.kilometerstand = kilometerstand;
+        }
+        public Auto(int kilometerstand, double verbrauchInLiternpro100K, double tankinhalt) : this(kilometerstand)
+        {
+            this.verbrauchInLiternpro100K = verbrauchInLiternpro100K;
+            this.tankinhalt = tankinhalt;
+        }
+        public void Fahren(int streckeInKilometer)
+        {
+            if (streckeInKilometer < 0)
+            {
+
+                throw new ArgumentOutOfRangeException();
+
+            }
+            else
+            {
+
+
+                Tankinhaltberechenen(streckeInKilometer);
+
+                if (i == 1)
+                {
+
+                   kilometerstand= kilometerstand + Convert.ToInt32(MaximaleKilometer());
+
+
+                }
+                else
+                {
+                    kilometerstand += streckeInKilometer;
+
+
+                }
+            }
+
+        }
+        public int Kilometerstand
+        { get => kilometerstand; }
+        private void Tankinhaltberechenen(double streckeInKilometer)
+        {
+            double ergebniss = streckeInKilometer / 100 * verbrauchInLiternpro100K;
+            if (ergebniss >= tankinhalt)
+            {
+                i++;
+
+                MaximaleKilometer();
+
+                //throw new ArgumentOutOfRangeException();
+               
+                
+            }
+            else
+            {
+                tankinhalt = tankinhalt - ergebniss;
+            }
+
+
+        }
+        public double Tankinhalt2
+        {
+
+            get => tankinhalt;
+
+        }
+        public double MaximaleKilometer()
+        {
+
+            double maximalereichweite = (tankinhalt / verbrauchInLiternpro100K) * 100;
+
+            return maximalereichweite;
+        }
+
+    }
+
+
+
+}
+
---
+++ FuhrplanVerwaltung/FuhrplanverwaltungTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FuhrplanverwaltungTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("FuhrplanverwaltungTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0f347160-2434-40a7-a53b-4a151cf3a350")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
